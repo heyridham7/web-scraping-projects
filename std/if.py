13701f7f71abf1e6bd77191049ea87783b3f4ef6
Ridham Patel <ridhamp@Ridhams-MacBook-Air.local>
2026-06-20 13:22:27 +0530

    Clean up and rename project files

--- std/if.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Entet A number:"))
-if num%7 ==0 and num>0:
-    print("The Number is multiple of 7")
-else:
-    print("The Number is not multiple of 7")
--- std/tup.py
+++
@@ -1,5 +0,0 @@
-cpi = (7.5 , 6.2 , 7.76 , 7.5)
-print(cpi)
-sal = ()
-print(sal)
-print(cpi[1:3])
